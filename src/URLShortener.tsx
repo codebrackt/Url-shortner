47fe10d32a7e4f7adc074d7d226a635413499f87
Anurag <anurag@infinitysoftsystems.com>
2023-07-13 11:26:02 +0530

    saving the links to the section

--- src/URLShortener.tsx
+++ src/URLShortener.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 import axios from "axios";
+import { TextField, Button, Typography } from "@mui/material";
+// we are using material you to make the ui more interactive
 
 const URLShortener: React.FC = () => {
   // State variables to hold the shortened link and user input
   const [shortenedLink, setShortenedLink] = useState("");
   const [userInput, setUserInput] = useState("");
+  // using this to show the saved links 
   const [savedLinks, setSavedLinks] = useState<string[]>([]);
 
   useEffect(() => {
@@ -66,41 +69,53 @@ const URLShortener: React.FC = () => {
   return (
     <div className="container h-screen flex justify-center items-center">
       <div className="text-center">
-        <h1 className="text-2xl font-medium text-blue-500 mb-4">
-          Our <span className="text-yellow-400">URL Shortener</span>
-        </h1>
+        <Typography variant="h4" color="green" gutterBottom>
+          Infinity <span className="text-red-400">URL Shortener</span>
+        </Typography>
         <div>
           {/* Input field for the user to enter the link */}
-          <input
-            className="outline-none border-2 border-blue-500 rounded-md backdrop-blur-xl bg-white/20 shadow-md px-3 py-3"
-            type="text"
-            placeholder="Enter link to be shortened"
+          <TextField
+            label="Enter link to be shortened"
+            variant="outlined"
+            size="small"
             value={userInput}
             /*
            The onChange event handler is used to capture changes that occur in an input element, such as a text field or a dropdown menu. In this case, it is being used in the context of a user input field.
            */
             onChange={handleInputChange}
           />
-          <button
-            className="bg-blue-500 text-white px-8 py-3 ml-4 rounded-md"
+          <Button
+            variant="contained"
+            color="primary"
             onClick={fetchData}
+            sx={{ ml: 2 }}
           >
             Submit URL
-          </button>
+          </Button>
           <div className="mt-5">
             {shortenedLink && (
               <>
                 <div>{shortenedLink}</div>
-                <CopyToClipboard text={shortenedLink} onCopy={handleCopyToClipboard}>
-                  <button className="border-2 border-blue-500 text-blue-500 font-medium px-5 py-2 ml-4 rounded-md">
+                <CopyToClipboard
+                  text={shortenedLink}
+                  onCopy={handleCopyToClipboard}
+                >
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    size="small"
+                    sx={{ ml: 2 }}
+                  >
                     Copy URL to Clipboard
-                  </button>
+                  </Button>
                 </CopyToClipboard>
               </>
             )}
           </div>
           <div className="mt-5">
-            <h2>Saved Links:</h2>
+            <Typography variant="h6" gutterBottom>
+              Saved Links:
+            </Typography>
             {savedLinks.map((link, index) => (
               <div key={index}>{link}</div>
             ))}
